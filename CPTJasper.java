5f7dc4229baac06d78958a1ce701d648a3e56f32
Jasper-Hung <jasper.hung26@ycdsbk12.ca>
2025-05-23 09:36:36 -0400

    Update CPTJasper.java

--- CPTJasper.java
+++ CPTJasper.java
@@ -4,5 +4,11 @@ public class CPTJasper{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello World");
+		
+		int intNum;
+		int intAdd;
+		
+		con.println("What is the number?: ");
+		intNum = con.readInt();
 	}
 }
